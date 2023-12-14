adba885f8b0ef70e207760315d20c5fe534ce6de
Wade Coats Jr. <149808971+WCC901@users.noreply.github.com>
2023-12-14 18:12:11 -0500

    Added an event listener to the button and figured out how to get the value needed from the dropdown menu.

---
+++ index.js
@@ -0,0 +1,20 @@
+// const input = (document.getElementById("eras")).querySelector("option");
+const gallery = document.getElementById("gallery");
+const button = document.querySelector("input");
+const body = document.querySelector("body");
+const select = document.querySelector("select");
+
+console.log(input.value);
+
+function handleClick(){
+    if (select.value == "jurassic") {
+        
+    }
+    else if (select.value == "cretaceous") {
+        
+    } else if (select.value == "triassic") {
+        
+    }
+}
+
+button.addEventListener("click", handleClick);
