e141de318273de1d5123d799136641b821e3110e
Wade Coats Jr. <149808971+WCC901@users.noreply.github.com>
2023-12-14 23:32:33 -0500

    Added handleBtnClick function to change the pictures according to the dropdown menu input, still has to be worked on

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 // const input = (document.getElementById("eras")).querySelector("option");
 const gallery = document.getElementById("gallery");
+const picsList = gallery.querySelectorAll("img");
 const button = document.querySelector("input");
 const body = document.querySelector("body");
 const select = document.querySelector("select");
 
-console.log(input.value);
+console.log (picsList);
 
-function handleClick(){
+// Function that should
+// update the image gallery
+// accoriding to the dropdown input
+function handleBtnClick(){
     if (select.value == "jurassic") {
-        
+        for (const picture of picsList) {
+            console.log(picture);
+            if (picture.src != "./apato.jpg") {
+                picture.setAttribute("src", "./apato.jpg");
+                picture.setAttribute("alt", "Apatosaurus Picture");
+            } else if (picture.src != "./dimo.jpg") {
+                picture.setAttribute("src", "./dimo.jpg");
+                picture.setAttribute("alt", "Dimorphodon Picture");
+            } else if (picture.src != "./stego.jpg") {
+                picture.setAttribute("src", "./stego.jpg");
+                picture.setAttribute("alt", "Stegosaurus Picture");
+            }
+        }
     }
-    else if (select.value == "cretaceous") {
+    else if (select.value == "triassic") {
         
-    } else if (select.value == "triassic") {
+    } else if (select.value == "cretaceous") {
         
     }
 }
 
-button.addEventListener("click", handleClick);
+button.addEventListener("click", handleBtnClick);
